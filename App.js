0c8d43791b2453939fe4d9570989bca20f67133c
Tristen Roupp <tristenr@usf.edu>
2024-06-28 21:40:40 -0400

    Index.html and index.js stucture setup

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ProductList from './components/ProductList';
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        axios.get('https://course-api.com/react-store-products')
+            .then(response => {
+                setProducts(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError('Failed to load products');
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div className="app">
+            <ProductList products={products} />
+        </div>
+    );
+};
+
+export default App;
